d1a1b3c8de608acee0a46fac57b930a70b7dcf15
Natalia Alferova <natalia@alferova.me>
2022-02-17 13:31:56 +0100

    Version 1

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.conditions.Visible;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.BACK_SPACE;
+
+public class CardDeliveryTest {
+    @Test
+    public void shouldBeSuccess () {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+        Date date = new Date(System.currentTimeMillis() + 3*24*60*60*1000);
+
+        open("http://localhost:9999");
+        Configuration.browser ="chrome";
+        Configuration.holdBrowserOpen=true;
+        $ ("[placeholder='Город']").setValue("Ульяновск");
+        $ ("[placeholder='Дата встречи']").doubleClick();
+        $ ("[placeholder='Дата встречи']").sendKeys(BACK_SPACE);
+        $ ("[placeholder='Дата встречи']").setValue(formatter.format(date));
+        $ ("[name='name']").setValue("Иванова Ольга");
+        $ ("[name='phone']").setValue("+79999999999");
+        $ ("[data-test-id='agreement']").click();
+        $ ("[class='button__text']").click();
+        $ ("div[class *= 'visible']").shouldBe(Visible.appear, Duration.ofSeconds(15));
+    }
+}
