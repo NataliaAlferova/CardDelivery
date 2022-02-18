dd95982a4cf725e7279d9b2efe6df71eede6d0dd
Natalia Alferova <natalia@alferova.me>
2022-02-18 13:06:37 +0100

    Version 4

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,32 +1,34 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.conditions.Visible;
 import org.junit.jupiter.api.Test;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Date;
-
-import static com.codeborne.selenide.Selectors.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.Keys.*;
 
 public class CardDeliveryTest {
+    String planningDate = generateDate(3);
+
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
-    public void shouldBeSuccess () {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-        Date date = new Date(System.currentTimeMillis() + 3*24*60*60*1000);
+    public void shouldBeSuccess() {
 
         open("http://localhost:9999");
-//        Configuration.browser ="chrome";
-//        Configuration.holdBrowserOpen=true;
-        $ ("[placeholder='Город']").setValue("Ульяновск");
-        $ ("[placeholder='Дата встречи']").doubleClick();
-        $ ("[placeholder='Дата встречи']").sendKeys(BACK_SPACE);
-        $ ("[placeholder='Дата встречи']").setValue(formatter.format(date));
-        $ ("[name='name']").setValue("Иванова Ольга");
-        $ ("[name='phone']").setValue("+79999999999");
-        $ ("[data-test-id='agreement']").click();
-        $ ("[class='button__text']").click();
-        $ ("div[class *= 'visible']").shouldBe(Visible.appear, Duration.ofSeconds(15));
+        Configuration.browser = "chrome";
+        Configuration.holdBrowserOpen = true;
+        $("[placeholder='Город']").setValue("Ульяновск");
+        $("[placeholder='Дата встречи']").doubleClick();
+        $("[placeholder='Дата встречи']").sendKeys(BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(planningDate);
+        $("[name='name']").setValue("Иванова Ольга");
+        $("[name='phone']").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $("[class='button__text']").click();
+        $("[class='notification__title']").shouldHave(Condition.text("Встреча успешно забронирована на" + planningDate),
+                Duration.ofSeconds(15));
     }
 }
